c81693c9b4c32d7e530b826e211ed67403541e6d
csarikonda <csarikonda@uworld.in>
2022-03-01 12:13:51 +0530

    directives example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  redColor = false;
+  headerTag='h4';
+  ngModelText = '';
+  condition = false;
+  displayText=true;
+
+  numbers = [ ...Array(10).keys() ];
+
+  currentStyles = {
+    'font-style':  'italic',
+    'font-size':   '24px',
+  };
+
   title = 'directives-example';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './directives/Highlight/highlight.directive';
+import { UnlessDirective } from './directives/Unless/unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective,
+    UnlessDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
